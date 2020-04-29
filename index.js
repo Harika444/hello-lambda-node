3635e541c9d25b5ddfd72fcaa272e80c235ce0e6
Harika444 <47811739+Harika444@users.noreply.github.com>
2020-04-29 15:58:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hi from some other change check stage Lambda!'),
+        body: JSON.stringify('Hi from some other change check stage change Lambda!'),
 
     };
     return response;
