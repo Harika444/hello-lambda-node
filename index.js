1eaf2c0a51301480f9c2f8e772e699459ece09db
Harika444 <47811739+Harika444@users.noreply.github.com>
2020-04-29 10:44:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hi from harika $env stage Lambda!'),
+        body: JSON.stringify('Hi from harika some change stage Lambda!'),
 
     };
     return response;
