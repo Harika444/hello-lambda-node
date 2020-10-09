7c34e16fbf4635cae5af37b328329425d2360352
Harika444 <47811739+Harika444@users.noreply.github.com>
2020-10-09 14:14:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hi!'),
+        body: JSON.stringify('Hi Girl!'),
 
     }; 
     return response;
